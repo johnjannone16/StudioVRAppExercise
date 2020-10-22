4192537f49c9bca0e3d03c89b11a2a8f8e405ffd
johnjannone16 <68834847+johnjannone16@users.noreply.github.com>
2020-10-22 12:15:31 -0400

    Last Push for Proj i think

--- PortalMuseum/Assets/Scripts/EventController.cs
+++ PortalMuseum/Assets/Scripts/EventController.cs
@@ -7,12 +7,33 @@ using UnityEngine.Video;
 public class EventController : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
+    public Animator rotAnimation;
+    public bool animation_bool;
+
 
     private void Awake()
     {
         videoPlayer = GetComponent<VideoPlayer>();
+        rotAnimation = GetComponent<Animator>();
+        animation_bool = false;
 
     }
+
+
+
+
+    public void Rotation(Hand hand)
+    {
+        rotAnimation.SetBool("rotationBool", true);
+        animation_bool = true; 
+    }
+
+    public void rotOff(Hand hand)
+    {
+        rotAnimation.SetBool("rotationBool", false);
+        animation_bool = false;
+    }
+    
 
     public void OnPress(Hand hand)
     {
